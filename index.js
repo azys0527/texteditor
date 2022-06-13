b76971e9eb58437b8fc409d32708d106681057ae
yusuke <yusuke@hinako-iMac.local>
2022-06-14 00:06:32 +0900

    innerHTML=>textBuffers

--- index.js
+++ index.js
@@ -1,73 +1,62 @@
 const input = document.querySelector("input");
 const log = document.getElementById("log");
+const carretElem = document.getElementById("carret");
 
-input.onkeydown = logKey;
+input.onkeydown = handleKeyDown;
 
-let address = 0;
-let htmladdress = 0;
-let wordcount = 0;
-
-function logKey(e){
-    if (e.key.length == 1){
-        //通常のアルファベットを入力した場合
-        //現在地と全体の文字数を+1する
-        address += 1;
-        htmladdress += 1;
-        wordcount += 1;
-        //現在地に文字を挿入する
-        log.innerHTML = log.innerHTML.slice(0,address - 1) + `${e.key}` + log.innerHTML.slice(address - 1);
-    } else if (e.key === "ArrowLeft"){
-        //左入力した場合
-        console.log(log,log.innerHTML.slice(0,address),log.innerHTML.slice(address))
-        //html上、現在地が先頭でないときの処理
-        if (log.innerHTML.slice(htmladdress - 4,htmladdress) === "<br>"){
-            htmladdress -= 4
-        } else if (htmladdress !== 0){
-            htmladdress -= 1
-        }
-        //表示上、現在地が先頭でないなら、現在地を-1する
-        if (address !== 0){
-            address -= 1;
-        }
-    } else if (e.key === "ArrowRight"){
-        //右入力した場合
-        console.log(log,log.innerHTML.slice(0,address),log.innerHTML.slice(address))
-        //html上、現在地が末尾でないときの処理
-        if (log.innerHTML.slice(htmladdress,htmladdress + 4) === "<br>"){
-            htmladdress += 4
-        } else if (htmladdress < log.innerHTML.length){
-            htmladdress += 1
-        }
-        //現在地が末尾でないなら、現在地を+1する
-        if (address !== wordcount){
-            address += 1;
-        }
-    } else if (e.key === "Backspace"){
-        //バックスペースを押した場合
-        //html上の処理
-        if (log.innerHTML.slice(htmladdress - 4,htmladdress) === "<br>"){
-            log.innerHTML = log.innerHTML.slice(0, htmladdress - 4) + log.innerHTML.slice(htmladdress + 1);
-            htmladdress -= 4;
-        } else if (htmladdress !== 0){
-                htmladdress -= 1;}
-        //表示上の処理
-        if (address !== 0){
-            address -= 1;
-            wordcount -= 1;
-            log.innerHTML = log.innerHTML.slice(0, address) + log.innerHTML.slice(address + 1);  
-        }
-        console.log(log)
-    } else if (e.key === "Enter"){
-        //改行を押した場合
-        console.log("Enter押したよ")
-        let a = log.innerHTML.slice(0,htmladdress);
-        let b = log.innerHTML.slice(htmladdress);
-        console.log(a,b)
-        log.innerHTML = a + "<br>" + b;
-        console.log(log , log.innerHTML.length, address)
+const textBuffers = [[]];
+const carretPos = { col: 0, row: 0 };
+function setCarretPosDiff(colDiff = 0, rowDiff = 0) {
+    carretPos.row += rowDiff;
+    carretPos.col += colDiff;
+    if (carretPos.row > textBuffers.length - 1) {
+        carretPos.row = textBuffers.length - 1;
     }
-    //console.log("全体の文字数は" + wordcount)
-    //console.log("表示上の現在地は" + address)
-    //console.log("表示上の現在地は" + log.innerHTML.slice(address - 1,address) + "と" +log.innerHTML.slice(address , address + 1) + "の間にいます")
-    console.log("html上の現在地は" + log.innerHTML.slice(htmladdress - 1,htmladdress) + "と" +log.innerHTML.slice(htmladdress , htmladdress + 1) + "の間にいます")
+    if (carretPos.col > textBuffers[carretPos.row].length - 1) {
+        carretPos.col = textBuffers[carretPos.row].length - 1
+    }
+    if (carretPos.row < 0) carretPos.row = 0;
+    if (carretPos.col < 0) carretPos.col = 0;
+}
+
+function handleKeyDown(e) {
+    const isChar = e.key.length === 1;
+    const keyName = e.key;
+    if (isChar) {
+        carretPos.col += 1;
+        textBuffers[carretPos.row].splice(carretPos.col, 0, keyName);
+    } else {
+        switch (keyName) {
+            case "ArrowLeft":
+                setCarretPosDiff(-1, 0);
+                break;
+            case "ArrowRight":
+                setCarretPosDiff(1, 0);
+                break;
+            case "ArrowUp":
+                setCarretPosDiff(0, -1);
+                break;
+            case "ArrowDown":
+                setCarretPosDiff(0, 1);
+                break;
+            case "Enter":
+                carretPos.row += 1;
+                textBuffers.splice(carretPos.row, 0, []);
+                setCarretPosDiff(0, 0);
+                break;
+            default:
+                break;
+        }
+    }
+    render();
+}
+
+function render() {
+    console.log(textBuffers, carretPos);
+    // map: Array<Array<Char>> => Array<String> 
+    // Array<String> => String
+    // [1,2,3,4].join("|") -> "1|2|3|4"
+    log.innerHTML = textBuffers.map(line => line.join("")).join("<br/>");
+    carretElem.style.left = `${carretPos.col * 10}px`;
+    carretElem.style.top = `${carretPos.row * 1.2}rem`;
 }
